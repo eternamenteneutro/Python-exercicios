4fb97b540d054c8896f7977e66901d73b86ee902
Daniel Santana <148571761+eternamenteneutro@users.noreply.github.com>
2025-01-31 09:18:37 -0300

    exercicio 1

---
+++ Lambda.py
@@ -0,0 +1,9 @@
+# A little anonimous function, that haven't a name, get the args but only can run an expression;
+'''x = lambda a: a*3
+print(f'A multiplicacão por 3 vale {x(int(input('digite um valor: ')))}')
+
+soma = lambda a, b: a+b
+print(f"A soma vale {soma(int(input('digite um valor: ')),int(input('digite outro valor: ')))}")
+'''
+graus_celsius = lambda f: (5/9)*(f-32)
+print(f'A converção para graus celsius fica {graus_celsius(float(input("Digite a temperatura: ")))}°C')
